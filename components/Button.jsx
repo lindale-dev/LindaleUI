20a88a8b347f30d972ae9b09d8a490d2612120c0
Merwan Achibet <achibet.merwan@gmail.com>
2017-11-04 10:09:22 +0100

    Clean up Button component

--- components/Button.jsx
+++ components/Button.jsx
@@ -5,21 +5,23 @@ function Button(props)
 {
     return (
         <div className='option_element row'>
-            <a className={'col s'+props.width+' offset-s'+props.offset+' waves-effect btn-flat-inline'} onClick={props.onClick}>{props.name}</a>
+            <a className={`col s${props.width} offset-s${props.offset} waves-effect btn-flat-inline`} onClick={props.onClick}>
+                {props.name}
+            </a>
         </div>
     );
 }
 
 Button.propTypes = {
     name: PropTypes.string.isRequired,
-	width: PropTypes.number,
-	offset: PropTypes.number,
-    onClick: PropTypes.func.isRequired
+    onClick: PropTypes.func.isRequired,
+    width: PropTypes.number,
+    offset: PropTypes.number
 };
 
 Button.defaultProps = {
-	width: 12,
-	offset: 0
+    width: 12,
+    offset: 0
 };
 
 export default Button;
