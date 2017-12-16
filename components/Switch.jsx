7175ef1cce95b9e05a5e254576213b5e35e64e3a
Thomas Hauchecorne <thomash76@gmail.com>
2017-12-16 21:14:14 +0100

    Adding Switch CSS

--- components/Switch.jsx
+++ components/Switch.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import uuid from 'uuid/v4';
 
+import './Switch.scss';
+
 // Simple switch
 function Switch(props)
 {
     return (
-        <div class="switch">
+        <div className="switch">
             <label>
                 <input type='checkbox' disabled={props.disabled} checked={props.checked} onChange={props.onChange.bind(null, !props.checked)} />
                 <span className='lever'></span>
