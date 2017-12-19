92922ff3399fd42cd5be6bda97794f181d1afb3c
Thomas Hauchecorne <thomash76@gmail.com>
2017-12-19 20:44:54 +0100

    Adding color palette

---
+++ colors.jsx
@@ -0,0 +1,21 @@
+import palette from './colors.scss';
+
+const colors = {
+    50: palette.primaryColor50,
+    100: palette.primaryColor100,
+    200: palette.primaryColor200,
+    300: palette.primaryColor300,
+    400: palette.primaryColor400,
+    500: palette.primaryColor500,
+    600: palette.primaryColor600,
+    700: palette.primaryColor700,
+    800: palette.primaryColor800,
+    900: palette.primaryColor900,
+    A100: palette.primaryColorA100,
+    A200: palette.primaryColorA200,
+    A400: palette.primaryColorA400,
+    A700: palette.primaryColorA700,
+    'contrastDefaultColor': palette.primaryColorContrast,
+};
+
+export default colors;
