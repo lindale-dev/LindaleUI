14494f8b21797c96a863ad6fc2bbad8d92d0310d
Thomas Hauchecorne <thomash76@gmail.com>
2018-01-05 22:21:44 +0100

    Adding Slider component

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -1,63 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import noUiSlider from 'nouislider';
 
+import RCSlider from 'rc-slider';
 import {numberUnitPropArray} from '../utils/customProps';
+import Tooltip from './Tooltip';
 
-function toNumber(x)
-{
-    return parseFloat(x);
-}
+import 'rc-slider/assets/index.css';
+import './Slider.scss';
 
-class Slider extends React.Component
-{
-    render()
-    {
-        return (
-            <div className='option_element row uniform grid'>
-                <label className='col s5'>{this.props.name}</label>
-                <div className='col s7'>
-                    <div className='left slider_label pointer_transparent'><i>{this.props.unit}</i></div>
-                    <div className='right slider_label'>
-                        <div className='slider_input_placeholder'>{toNumber(this.props.values[0]).toFixed(2)}</div>
-                    </div>
-                    <div className='input_slider adaptive_slider' ref={this.initSlider.bind(this)}></div>
-                </div>
-            </div>
-        );
-    }
-
-    initSlider(element)
-    {
-        if (element && !element.noUiSlider)
-        {
-            noUiSlider.create(element, {
-                start: this.props.values,
-                connect: this.props.connect,
-                behaviour: 'snap',
-                range: {
-                    min: toNumber(this.props.min),
-                    max: toNumber(this.props.max)
-                }
-            });
-
-            element.noUiSlider.on('end', (values, handle, rawValues) => {
-                this.props.onChange[handle](rawValues[handle]);
-            });
-        }
-    }
+// TODO range slider
+function Slider(props){
+    return(
+        <RCSlider defaultValue={props.values[0]}
+                  min={props.min}
+                  max={props.max}
+                  step={props.step}
+                  onAfterChange={props.onChange[0]} />
+    )
 }
 
 Slider.propTypes = {
-    name: PropTypes.string.isRequired,
     values: numberUnitPropArray,
-    min: PropTypes.number.isRequired,
-    max: PropTypes.number.isRequired,
-    onChange: PropTypes.arrayOf(PropTypes.func).isRequired,
-    connect: PropTypes.arrayOf(PropTypes.bool).isRequired
+    min: PropTypes.number,
+    max: PropTypes.number,
+    step: PropTypes.number,
+    onChange: PropTypes.arrayOf(PropTypes.func).isRequired
 };
 
 Slider.defaultProps = {
-}
+    min: 0,
+    max: 100,
+    step: 0.01
+};
 
 export default Slider;
