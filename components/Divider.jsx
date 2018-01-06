25da2f56b3ba390dc4f9f7ecd474762499c59701
Thomas Hauchecorne <thomash76@gmail.com>
2018-01-06 23:09:33 +0100

    Adding Divider component

---
+++ components/Divider.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Divider.scss';
+
+function Divider(props){
+    return(
+        <div className='divider'>
+        </div>
+    )
+}
+
+Divider.propTypes = {
+};
+
+export default Divider;
