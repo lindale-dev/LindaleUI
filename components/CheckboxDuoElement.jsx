0e97fa08ba3c418e54b22a01220dc8f8b9c6b703
Thomas Hauchecorne <thomash76@gmail.com>
2018-01-08 20:36:56 +0100

    Adding CheckboxDuoElement

---
+++ components/CheckboxDuoElement.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {FormControlLabel} from 'material-ui/Form';
+import Grid from 'material-ui/Grid';
+
+import Checkbox from './Checkbox';
+import ParameterElement from './ParameterElement';
+import './CheckboxDuoElement.scss';
+
+// Two checkboxes on the same line, with separate sub-labels
+function CheckboxDuoElement(props)
+{
+    return(
+        <ParameterElement name={props.name} tooltip={props.tooltip}>
+            <Grid container spacing={0} className='checkbox-duo-container'>
+                <Grid item xs={6} className='checkbox-duo-col'>
+                    <Checkbox className='small checkbox-duo-checkbox' 
+                                checked={props.checkedValues[0]} 
+                                onChange={props.onChange[0]} />
+                    <span className='checkbox-duo-label'>{props.labels[0]}</span>
+                </Grid>
+                <Grid item xs={6} className='checkbox-duo-col'>
+                    <span className='checkbox-duo-label checkbox-duo-label-right'>{props.labels[1]}</span>
+                    <Checkbox className='small checkbox-duo-checkbox' 
+                                checked={props.checkedValues[1]} 
+                                onChange={props.onChange[1]} />
+                </Grid>
+            </Grid>
+        </ParameterElement>
+    )
+}
+
+CheckboxDuoElement.propTypes = {
+    name: PropTypes.string.isRequired,
+    labels: PropTypes.arrayOf(PropTypes.string).isRequired,
+    checkedValues: PropTypes.arrayOf(PropTypes.bool).isRequired,
+    onChange: PropTypes.arrayOf(PropTypes.func).isRequired,
+    tooltip: PropTypes.string
+};
+
+CheckboxDuoElement.defaultProps = {
+    tooltip: ''
+};
+
+export default CheckboxDuoElement;
