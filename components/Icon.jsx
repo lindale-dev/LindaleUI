dd1140bdc4f678ac5ed267bbf6d15c61ecaae0cc
Thomas Hauchecorne <thomash76@gmail.com>
2018-01-15 19:50:51 +0100

    Adding tooltip on Icon

--- components/Icon.jsx
+++ components/Icon.jsx
@@ -5,12 +5,17 @@ import uuid from 'uuid/v4';
 import MUIIcon from 'material-ui/Icon';
 import { withStyles } from 'material-ui/styles';
 
+import Tooltip from './Tooltip';
+
 const style = {
     root: {
         '.small &, &.small':{
             height: 18,
             width: 18,
             fontSize: 18,
+        },
+        '.forbidden &, &.forbidden':{
+            cursor: 'not-allowed',
         }
     },
 };
@@ -18,18 +23,31 @@ const style = {
 // Simple icon with conditonnal small size
 function Icon(props)
 {
+    let color=null;
+    if(props.color){
+        color={color: props.color};
+    }
+
     return (
-        <MUIIcon
-            className={props.className}
-            classes={ { root: props.classes.root, } }
-        >
-            {props.icon}
-        </MUIIcon>
+        <Tooltip title={props.tooltip}>
+            <MUIIcon
+                className={props.className}
+                style={color}
+                classes={ { root: props.classes.root, } }
+            >
+                {props.icon}
+            </MUIIcon>
+        </Tooltip>
     );
 }
 
 Icon.propTypes = {
     icon: PropTypes.string.isRequired,
+    tooltip: PropTypes.string
+};
+
+Icon.defaultProps = {
+    tooltip: ''
 };
 
 export default withStyles(style)(Icon);
