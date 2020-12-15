821f4792e3b2f11b8c217729138ab5e5fe3bd31b
Thomas <thomash76@gmail.com>
2020-12-15 18:18:03 +0100

    Button Adding loading progress

--- components/Button.tsx
+++ components/Button.tsx
@@ -16,15 +16,33 @@ const useStyles = MUI.makeStyles((theme: MUI.Theme) =>
                 padding: '1px 8px 0px',
             },
         },
-        progressWrapper: {
+        wrapper: {
             position: 'relative',
         },
+
+        hiddenButtonLabel: {
+            visibility: 'hidden',
+        },
+
         progress: {
             position: 'absolute',
             top: '50%',
             left: '50%',
             marginTop: -12,
             marginLeft: -12,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        progressLabel: {
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+            position: 'absolute',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
         },
     })
 );
@@ -35,6 +53,7 @@ type Props = Omit<MUI.ButtonProps, 'size'> &
     tooltip?: TooltipProps['title'],
     MUISize?: MUI.ButtonProps['size'],
     loading?: boolean,
+    loadingProgress?: number,
     size?: number
 };
 
@@ -44,12 +63,13 @@ export default function Button(props: Props)
 
     return (
         <Tooltip title={props.tooltip ?? ''}>
-            <div className={classnames(classes.progressWrapper, props.className)}>
+            <MUI.Box className={classnames(classes.wrapper, props.className)}>
+
                 <MUI.Button
-                    classes={ { root: classes.root, } }
+                    classes={ { root: classes.root, label: props.loading && props.loadingProgress !== undefined ? classes.hiddenButtonLabel : undefined} }
                     className={props.size ? 'size'+props.size.toString()+' ' : undefined}
                     color={props.color}
-                    disabled={props.disabled}
+                    disabled={props.disabled || props.loading}
                     fullWidth={props.fullWidth}
                     onClick={props.onClick}
                     size={props.MUISize}
@@ -57,8 +77,20 @@ export default function Button(props: Props)
                 >
                     {props.children}
                 </MUI.Button>
-                {props.loading && <MUI.CircularProgress size={24} className={classes.progress} />}
-            </div>
+
+                {props.loading &&
+                    <MUI.Box className={classes.progress}>
+                        <MUI.CircularProgress size={24} variant={props.loadingProgress !== undefined ? 'determinate' : 'indeterminate'} value={props.loadingProgress} />
+
+                        {props.loadingProgress !== undefined &&
+                            <MUI.Box className={classes.progressLabel}>
+                                <MUI.Typography variant='caption' component='div' color='textSecondary'>{`${Math.round(props.loadingProgress)}%`}</MUI.Typography>
+                            </MUI.Box>
+                        }
+                    </MUI.Box>
+                }
+
+            </MUI.Box>
         </Tooltip>
     );
 }
