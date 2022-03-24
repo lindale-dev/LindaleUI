c8791e0301ff99b42b6c6486af66c069318f0916
Merwan Achibet <achibet.merwan@gmail.com>
2022-03-24 16:56:04 +0100

    Build: clean up CRA config override

--- demo/config-overrides.js
+++ demo/config-overrides.js
@@ -7,13 +7,6 @@ module.exports = function override(config) {
   // Needed to import modules located outside of the app (esp. lindale-ui)
   config = removeModuleScopePlugin()(config);
 
-  // Needed or we get strange "Invalid Hook Call" errors due to mismatching React versions,
-  // even though the versions in the demo/package seem to match.
-  config = addWebpackAlias({
-    //react: path.resolve('./node_modules/react'),
-    //'react-dom': path.resolve('./node_modules/react-dom')
-  })(config);
-
   // Needed to parse Typescript files located outside of the app
   // https://stackoverflow.com/a/70256204
   // https://stackoverflow.com/questions/65893787/create-react-app-with-typescript-and-npm-link-enums-causing-module-parse-failed.
