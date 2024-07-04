91b430ab7a502dfc573c88a24f35661fe033f228
Merwan Achibet <achibet.merwan@gmail.com>
2024-07-04 09:19:14 +0200

    Utils: remove some @ts-ignore

--- utils/renderWhenLoaded.ts
+++ utils/renderWhenLoaded.ts
@@ -8,18 +8,13 @@ export function renderWhenLoaded(
 ) {
   window.addEventListener("load", () => {
     // Reacts discourages rendering directly at the body's root so we create a container for our dialog
+
     const container = document.createElement("div");
     container.id = "root";
-
     document.body.appendChild(container);
 
-    // To this date, these types definitions are still missing from @types/react-dom
-    /* eslint-disable @typescript-eslint/ban-ts-comment, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access */
-    // @ts-ignore
     const root = ReactDOM.createRoot(container);
-    // @ts-ignore
     root.render(element);
-    /* eslint-enable @typescript-eslint/ban-ts-comment, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access */
 
     if (callback) {
       callback();
