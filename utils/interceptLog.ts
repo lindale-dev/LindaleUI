8481cb6101edbf2c6b3478b2fc3f254e5fb4c56f
Merwan Achibet <achibet.merwan@gmail.com>
2024-07-17 09:59:55 +0200

    interceptLog: return the initial logging function

--- utils/interceptLog.ts
+++ utils/interceptLog.ts
@@ -2,20 +2,21 @@
 //
 // `logFunctionName`   name of one of the console logging functions
 // `logHandler`        callback that takes the logging level and then the same arguments as the initial logging call
-
 export type LogLevel = "log" | "debug" | "info" | "warn" | "error";
 
 export function interceptLog(
   logFunctionName: LogLevel,
   logHandler: (level: LogLevel, ...args: unknown[]) => void,
-) {
-  const logFunction = console[logFunctionName];
+): (...args: unknown[]) => void {
+  const initialLogFunction = console[logFunctionName];
 
   console[logFunctionName] = (...args: unknown[]) => {
     // Call the original log function
-    logFunction(...args);
+    initialLogFunction(...args);
 
     // Call the custom log handler with the log level as the first argument and then the actual arguments
     logHandler(logFunctionName, ...args);
   };
+
+  return initialLogFunction;
 }
